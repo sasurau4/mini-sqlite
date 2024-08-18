a862d2239b43a4319eb0fe35f85eac5806761148
Daiki Ihara <sasurau4@gmail.com>
2024-08-18 18:59:02 +0900

    feat: impl part6

--- db.c
+++ db.c
@@ -85,6 +85,13 @@ typedef struct
     Row row_to_insert; // only used by insert statement
 } Statement;
 
+typedef struct
+{
+    Table *table;
+    u_int32_t row_num;
+    bool end_of_table; // Indicates a position one past the last element
+} Cursor;
+
 InputBuffer *new_input_buffer()
 {
     InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
@@ -112,6 +119,26 @@ void deserialize_row(void *source, Row *destination)
     memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
     memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+Cursor *table_start(Table *table)
+{
+    Cursor *cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+Cursor *table_end(Table *table)
+{
+    Cursor *cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
 }
 
 void *get_page(Pager *pager, u_int32_t page_num)
@@ -266,13 +293,23 @@ void db_close(Table *table)
     free(table);
 }
 
-void *row_slot(Table *table, u_int32_t row_num)
+void *cursor_value(Cursor *cursor)
 {
+    u_int32_t row_num = cursor->row_num;
     u_int32_t page_num = row_num / ROWS_PER_PAGE;
-    void *page = get_page(table->pager, page_num);
+    void *page = get_page(cursor->table->pager, page_num);
     u_int32_t row_offset = row_num % ROWS_PER_PAGE;
     u_int32_t byte_offset = row_offset * ROW_SIZE;
     return page + byte_offset;
+}
+
+void cursor_advance(Cursor *cursor)
+{
+    cursor->row_num += 1;
+    if (cursor->row_num >= cursor->table->num_rows)
+    {
+        cursor->end_of_table = true;
+    }
 }
 
 void close_input_buffer(InputBuffer *input_buffer)
@@ -367,21 +404,29 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
     }
 
     Row *row_to_insert = &(statement->row_to_insert);
+    Cursor *cursor = table_end(table);
 
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement *statement, Table *table)
 {
+    Cursor *cursor = table_start(table);
     Row row;
-    for (u_int32_t i = 0; i < table->num_rows; i++)
+    while (!(cursor->end_of_table))
     {
-        deserialize_row(row_slot(table, i), &row);
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+
+    free(cursor);
+
     return EXECUTE_SUCCESS;
 }
 
